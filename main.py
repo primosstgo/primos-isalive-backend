613a0adbd7e34905e288a7a920c9bd6b7ffaf591
primosstgo <primiosstgo@gmail.com>
2023-04-26 17:26:13 +0000

    FastAPI CORS added

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import os
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from components import getContainerById, getContainers
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
